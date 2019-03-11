858238b62f228395a53a5e6b22364f600d808e5d
Tacoholic <solischristian@gmail.com>
2019-03-11 15:53:31 -0500

    added sessions controller all controllers added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,14 @@ class Restaurant < ApplicationRecord
 
   has_many :pictures, through: :users
   has_many :reviews, through: :users
+
+  geocoded_by :address
+  after_validation :geocode
+
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
+
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,32 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  namespace :api do
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  get  '/users' => 'users#index'
+  post '/users' => 'users#create'
+  get  '/users' => 'users#show'
+  patch '/users' => 'users#update'
+  delete '/users' => 'users#destroy'  
+
+  get  '/sessions' => 'sessions#index'
+  post '/sessions' => 'sessions#create'
+  get  '/sessions' => 'sessions#show'
+
+  get '/restaurants' => 'restaurants#index'
+  post '/restaurants' => 'restaurants#create'
+  get '/restaurants' => 'restaurants#show'
+  patch '/restaurants' => 'restaurants#update'
+  delete '/restaurants' => 'restaurants#destroy'
+
+  get 'reviews' => 'reviews#index'
+  post '/reviews' => 'reviews#create'
+  get '/reviews' => 'reviews#show'
+  patch '/reviews' => 'reviews#update'
+  delete '/reviews' => 'reviews#destroy'
+
+  get 'pictures' => 'pictures#index'
+  post '/pictures' => 'pictures#create'
+  get '/pictures' => 'pictures#show'
+  patch 'pictures' => 'pictures#update'
+  delete 'pictures' => 'pictures#destroy'
+  end
 end
