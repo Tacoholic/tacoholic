177c68197a518dbf6cc52ecc25e1f190a682ab23
Tacoholic <solischristian@gmail.com>
2019-04-17 11:11:37 -0500

    minor tweak to API call for index page

--- app/controllers/google/restaurants_controller.rb
+++ app/controllers/google/restaurants_controller.rb
@@ -3,19 +3,19 @@ class Google::RestaurantsController < ApplicationController
   def index
     response = RestClient::Request.execute(
         method: :get,
-        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.8921364,-87.6370182&radius=1800&type=restaurant,formatted_phone_number&keyword=tacos&key=API KEY',
-        headers: {api_key: 'API KEY'  }
+        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=41.8921364,-87.6370182&radius=1800&type=restaurant,formatted_phone_number&keyword=tacos&key=',
+        headers: {api_key: ''  }
       )
 
     @restaurants = JSON.parse(response)["results"]
-    render 'index.json.jbuilder' 
+    render json: @restaurants 
   end
 
   def show
     response = RestClient::Request.execute(
       method: :get,
-      url: "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{params[:place_id]}&fields=name,rating,formatted_phone_number,user_ratings_total,formatted_address,opening_hours,website,price_level&key=API KEY",
-     headers: {api_key: 'API KEY'}
+      url: "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{params[:place_id]}&fields=name,rating,formatted_phone_number,user_ratings_total,formatted_address,opening_hours,website,price_level&key=",
+     headers: {api_key: ''}
       )
 
     @restaurant = JSON.parse(response)["result"]
